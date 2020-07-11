5fde715e9e5335ec5662f7c0ba2fae7443a6bf9c
shibatasuguru0311 <s.shibata.stuvwx@gmail.com>
2020-07-11 11:38:21 +0900

    index-controller1

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
   root "shops#index"
   resources :shops 
   resources :items, only: [:new, :create] do
+    resources :messages, only:[:index, :create]
     collection do
       get 'search'
     end
   end
-  resources :messages 
-  
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+
 
